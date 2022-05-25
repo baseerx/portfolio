bc4c5128d028cdc4f6dae1b10225a241fb87ad18
baseerntdc <98307416+baseerntdc@users.noreply.github.com>
2022-05-25 17:36:23 +0500

    contactme image change

--- src/portfolio.js
+++ src/portfolio.js
@@ -411,7 +411,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "harikrushn.jpg",
+    profile_image_path: "baseer.jpeg",
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
